bdc072f871b9bcdd03374f066853049374a5ab12
johaanez <janez@baufest.com>
2016-09-26 03:40:53 -0300

    prueba

--- list.h
+++ list.h
@@ -15,5 +15,6 @@ package_t * list_getPackages(list_t *self);
 int list_getPackagesPerDay(list_t self, dateTime_t dt, package_t *package);
 int list_getSize(list_t self);
 
+
 #endif /* LISTA_H_ */
 ;
